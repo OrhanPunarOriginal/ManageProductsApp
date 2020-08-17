84fc40866a1657d12a19d7a9999054b90805f26f
OrhanPunar <orhan.punar@bouvet.no>
2020-08-17 13:59:07 +0000

    NextCommit

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -6,25 +6,26 @@ sap.ui.define([
 
 	return BaseController.extend("opensapManageProducts.ManageProducts.controller.App", {
 
-		onInit : function () {
+		onInit: function () {
+			var a;
 			var oViewModel,
 				fnSetAppNotBusy,
 				iOriginalBusyDelay = this.getView().getBusyIndicatorDelay();
 
 			oViewModel = new JSONModel({
-				busy : true,
-				delay : 0
+				busy: true,
+				delay: 0
 			});
 			this.setModel(oViewModel, "appView");
 
-			fnSetAppNotBusy = function() {
+			fnSetAppNotBusy = function () {
 				oViewModel.setProperty("/busy", false);
 				oViewModel.setProperty("/delay", iOriginalBusyDelay);
 			};
 
 			// disable busy indication when the metadata is loaded and in case of errors
 			this.getOwnerComponent().getModel().metadataLoaded().
-				then(fnSetAppNotBusy);
+			then(fnSetAppNotBusy);
 			this.getOwnerComponent().getModel().attachMetadataFailed(fnSetAppNotBusy);
 
 			// apply content density mode to root view
